7da341a5b173d8b58b583fac127597941c2f2eba
shahwaiz111 <shahwaizrazi544@gmail.com>
2024-09-01 00:34:01 +0500

    files added

---
+++ enroll/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import User
+# Register your models here.
+
+# admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+  list_display=('id','name','email','password')
+admin.site.register(User, UserAdmin)
